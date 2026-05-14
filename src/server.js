4703cd8260f222be51c7edddc1445f7e41f87cd8
Anand Koushik <your-email@example.com>
2026-05-14 14:39:38 +0530

    Update backend code

--- src/server.js
+++ src/server.js
@@ -13,19 +13,26 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// CORS CONFIG
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "blog-frontend-eight-sigma.vercel.app",
+      "https://blog-frontend-eight-sigma.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
+// HANDLE PREFLIGHT REQUESTS
+app.options("*", cors());
+
+// MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
 
+// HEALTH ROUTE
 app.get("/api/health", (req, res) => {
   res.json({
     success: true,
@@ -33,12 +40,15 @@ app.get("/api/health", (req, res) => {
   });
 });
 
+// ROUTES
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
+// ERROR HANDLERS
 app.use(notFound);
 app.use(errorHandler);
 
+// START SERVER
 const startServer = async () => {
   try {
     await connectDB();
@@ -47,7 +57,7 @@ const startServer = async () => {
       console.log(`Server running on port ${port}`);
     });
   } catch (error) {
-    console.log(error);
+    console.log("Server Error:", error);
   }
 };
 
